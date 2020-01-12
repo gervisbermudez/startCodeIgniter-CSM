6db3f071c5acf5560c3692997eea080b8139d5a8
Gervis Bermudez <gbermudezmora@quaresitsolutions.com>
2020-01-12 20:12:31 -0300

    Improve api user v1

--- application/controllers/api/v1/User.php
+++ application/controllers/api/v1/User.php
@@ -23,17 +23,43 @@ class User extends REST_Controller
      */
     public function index_get($id = 0)
     {
+        $where = "";
+
         if (!empty($id)) {
-            $data = $this->db->get_where("user", ['id' => $id])->row_array();
-        } else {
-            $data = $this->db->get("user")->result();
+            $where = "WHERE u.id = $id";
         }
+
+        $sql = "SELECT u.`id`,
+        u.`username`,
+        u.`email`,
+        u.`lastseen`,
+        u.`usergroup`,
+        u.`status`, CONCAT('{', GROUP_CONCAT(s.data), '}') AS `user_data`,
+        g.name AS `role`,
+        g.`level`
+        FROM (
+        SELECT d.id_user, CONCAT('\"', d._key, '\"', ':', '\"', d._value, '\"') AS `data`
+        FROM datauserstorage d
+        GROUP BY d.id) s
+        INNER JOIN `user` u ON u.id = s.id_user
+        INNER JOIN `usergroup` g ON u.usergroup = g.id
+        $where
+        GROUP BY s.id_user;";
+
+        $data = $this->db->query($sql)->result_array();
 
         if (!$data) {
             $data = array();
+            $this->response($data, REST_Controller::HTTP_NOT_FOUND);
+        }else{
+            foreach ($data as $key => &$value) {
+                $data_values = json_decode($value['user_data']);
+                $value['user_data'] = $data_values;
+            }
+    
+            $this->response($data, REST_Controller::HTTP_OK);
         }
 
-        $this->response($data, REST_Controller::HTTP_OK);
     }
 
     /**
