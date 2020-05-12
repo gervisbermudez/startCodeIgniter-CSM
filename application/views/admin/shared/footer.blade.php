4954a0fc724dcae6b765965d163fc0a6e9c9eec1
Gervis Bermudez <gbermudezmora@quaresitsolutions.com>
2020-05-12 20:29:06 -0300

    Fix footer

--- application/views/admin/shared/footer.blade.php
+++ application/views/admin/shared/footer.blade.php
@@ -4,9 +4,9 @@ const ENVIRONMENT = <?=json_encode(ENVIRONMENT)?>;
 const DEBUGMODE = <?=json_encode($ci->config->item('debug_mode'))?>;
 </script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
-<script src="{{base_url('public/js/jquery.js')}}"></script>
-<script src="<?php echo base_url(); ?>public/js/jquery.nicescroll.min.js"></script>
-<script src="{{base_url('public/js/start.min.js?v=' . SITEVERSION)}}"></script>
+<script src="{{base_url(JSPATH . 'jquery.js?v=' . SITEVERSION)}}"></script>
+<script src="{{base_url(JSPATH . 'jquery.nicescroll.min.js?v=' . SITEVERSION) }}"></script>
+<script src="{{base_url(JSPATH . 'start.min.js?v=' . SITEVERSION)}}"></script>
 @isset($footer_includes)
 	@foreach($footer_includes as $include)
 	<?php echo $include ?>
