dcfe716fb0ffcd1ce10c91ac39823d0ee43a5e82
Gervis Bermudez <gervisbermudez15@gmail.com>
2020-09-09 15:43:25 -0300

    fix: navbar

--- application/views/admin/shared/navbar.blade.php
+++ application/views/admin/shared/navbar.blade.php
@@ -2,8 +2,8 @@
     <div class="nav-wrapper">
         <a href="#" data-target="slide-out" class="sidenav-trigger show-on-medium-and-down"><i class="material-icons">menu</i></a>
         <a class='dropdown-trigger right' href='#' data-target='user_dropdown'>
-            @if ($ci->session->userdata('avatar'))
-            <img src="{{base_url(IMGPROFILEPATH) . $ci->session->userdata('username') . '/' . $ci->session->userdata('avatar') }}" alt="" class="circle z-depth-1" />
+            @if (userdata('avatar'))
+            <img src="{{base_url(IMGPROFILEPATH) . userdata('username') . '/' . userdata('avatar') }}" alt="" class="circle z-depth-1" />
             @else
             <i class="material-icons circle grey lighten-5 profile z-depth-1">account_circle</i>
             @endif
@@ -12,18 +12,18 @@
             <div class="user-view">
                 <div class="background">
                 </div>
-                <a href="{{base_url('admin/usuarios/ver/' . $ci->session->userdata('id')) }}" class="user-avatar">
-                    @if ($ci->session->userdata('avatar'))
-                    <img src="{{base_url(IMGPROFILEPATH) . $ci->session->userdata('username') . '/' . $ci->session->userdata('avatar') }}" alt="" class="circle z-depth-1" />
+                <a href="{{base_url('admin/usuarios/ver/' . userdata('user_id')) }}" class="user-avatar">
+                    @if (userdata('avatar'))
+                    <img src="{{base_url(IMGPROFILEPATH) . userdata('username') . '/' . userdata('avatar') }}" alt="" class="circle z-depth-1" />
                     @else
                     <i class="material-icons circle grey lighten-5 profile z-depth-1">account_circle</i>
                     @endif
                 </a>
-                <a class="avatar-username" href="{{base_url('admin/usuarios/ver/' . $ci->session->userdata('id')) }}">
-                    <span class="white-text name">{{$ci->session->userdata('username') }}</span>
+                <a class="avatar-username" href="{{base_url('admin/usuarios/ver/' . userdata('user_id')) }}">
+                    <span class="white-text name">{{userdata('username') }}</span>
                 </a>
                 <a class="avatar-email" href="#email">
-                    <span class="white-text email">{{$ci->session->userdata('type') }}</span>
+                    <span class="white-text email">{{userdata('type') }}</span>
                 </a>
             </div>
             <ul class="menu">
