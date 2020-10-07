5614a4796185c88c5544d7d8bee955f7b02e1f11
Gervis Bermudez <gervisbermudez15@gmail.com>
2020-10-07 17:58:40 -0300

    feature: UserGroups

--- application/models/Admin/Usergroup.php
+++ application/models/Admin/Usergroup.php
@@ -1,4 +1,8 @@
-<?php if (!defined('BASEPATH')) {
+<?php 
+
+use Tightenco\Collect\Support\Collection;
+
+if (!defined('BASEPATH')) {
     exit('No direct script access allowed');
 }
 
@@ -14,6 +18,38 @@ class Usergroup extends MY_model
     public function __construct()
     {
         parent::__construct();
+    }
+
+    public function filter_results($collection = [])
+    {
+        $this->load->model('Admin/Usergroup_permisions');
+        foreach ($collection as &$value) {
+            if (isset($value->usergroup_id)) {
+                $Usergroup_permisions = new Usergroup_permisions();
+                $value->{"usergroup_permisions"} = $Usergroup_permisions->where(["usergroup_id" => $value->usergroup_id]);
+            }
+        }
+
+        $this->load->model('Admin/User');
+        foreach ($collection as &$value) {
+            if (isset($value->user_id)) {
+                $user = new User();
+                $user->find($value->user_id);
+                $value->{'user'} = $user;
+            }
+        }
+
+        foreach ($collection as &$value) {
+            if (isset($value->parent_id) && $value->usergroup_id) {
+                $this->load->model('Admin/Usergroup');
+                $Usergroup = new Usergroup();
+                $sub_usergroups = $Usergroup->where(['parent_id' => $value->usergroup_id]);
+
+                $value->{'sub_usergroups'} = $sub_usergroups ? $sub_usergroups : [];
+            }
+        }
+
+        return new Collection($collection);
     }
 
 }
--- resources/js/start.js
+++ resources/js/start.js
@@ -35,6 +35,41 @@ var mixins = {
     },
   },
   methods: {
+    getSortData(strPropertyName) {
+      if (this.orderDataConf.strPropertyName == strPropertyName) {
+        return "sort_desc";
+      }
+      if (this.orderDataConf.strPropertyName == "-" + strPropertyName) {
+        return "sort_asc";
+      }
+      return "both";
+    },
+    sortData(strPropertyName, array) {
+      strPropertyName =
+        this.orderDataConf.strPropertyName == null
+          ? strPropertyName
+          : this.orderDataConf.strPropertyName == strPropertyName
+          ? "-" + strPropertyName
+          : strPropertyName;
+      let sorted = array.sort(this.dynamicSort(strPropertyName));
+      this.orderDataConf.strPropertyName = strPropertyName;
+      array = sorted;
+    },
+    dynamicSort(property) {
+      var sortOrder = 1;
+      if (property[0] === "-") {
+        sortOrder = -1;
+        property = property.substr(1);
+      }
+      return function (a, b) {
+        /* next line works with strings and numbers,
+         * and you may want to customize it to your needs
+         */
+        var result =
+          a[property] < b[property] ? -1 : a[property] > b[property] ? 1 : 0;
+        return result * sortOrder;
+      };
+    },
     base_url: function (path) {
       return BASEURL + path;
     },
@@ -183,9 +218,9 @@ class User {
   }
 
   get_fullname = () => {
-    try {
+    if (this.user_data.nombre && this.user_data.apellido) {
       return this.user_data.nombre + " " + this.user_data.apellido;
-    } catch (error) {
+    } else {
       return "";
     }
   };
