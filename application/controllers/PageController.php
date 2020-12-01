e311282b2c6260275c349adce9fa08018a330a51
Gervis Bermudez <gervisbermudez15@gmail.com>
2020-12-01 18:07:44 -0300

    rewrite PageController

--- application/controllers/PageController.php
+++ application/controllers/PageController.php
@@ -15,98 +15,36 @@ class PageController extends Base_Controller
 
     public function index()
     {
-        $pageInfo = new Page();
-        $result = $pageInfo->find_with(array('path' => $this->uri->uri_string(), 'status' => 1));
-
-        if (!$result) {
-            //Not found Page
+        $data = $this->get_page_info(array('path' => $this->uri->uri_string(), 'status' => 1));
+        if ($data == null) {
             $this->error404();
             return;
         }
-
-        //Is the page published?
-        $date_now = new DateTime();
-        $pagePublishTime = DateTime::createFromFormat('Y-m-d H:i:s', $pageInfo->date_publish);
-
-        if ($date_now < $pagePublishTime) {
-            //Not found Page
-            $this->error404();
-            return;
-        }
-
-        $data['page'] = $pageInfo;
-        $data['meta'] = $this->getPageMetas($pageInfo);
-        $data['title'] = $pageInfo->page_data["title"] ? config("SITE_TITLE") . " - " . $pageInfo->page_data["title"] : config("SITE_TITLE") . " - " . $pageInfo->title;
-        $data['layout'] = $pageInfo->layout == 'default' ? 'site' : $pageInfo->layout;
-        $data['headers_includes'] = isset($pageInfo->page_data["headers_includes"]) ? $pageInfo->page_data["headers_includes"] : "";
-        $data['footer_includes'] = isset($pageInfo->page_data["footer_includes"]) ? $pageInfo->page_data["footer_includes"] : "";
-        $template = $pageInfo->template == 'default' ? 'template' : $pageInfo->template;
-
-        /* if (getThemePath()) {
-        $this->blade->changePath(getThemePath());
-        } */
-
-        $this->load->model('Admin/Menu');
-        $menu = new Menu();
-        $menu->find_with(['menu_id' => 1]);
-
-        $data["menu"] = $menu;
-
         if (getThemePath()) {
             $this->blade->changePath(getThemePath());
         }
 
-        echo $this->blade->view("site.templates." . $template, $data);
+        echo $this->blade->view("site.templates." . $data["template"], $data);
 
     }
 
     public function home()
     {
-        $home_page = config("SITE_HOME_PAGE");
-        if($home_page){
-            $pageInfo = new Page();
-            $result = $pageInfo->find_with(array('page_id' => $home_page, 'status' => 1));
-
-            if (!$result) {
-                //Not found Page
+        //Check if there are a page configured as home page
+        $page_id = config("SITE_HOME_PAGE");
+        if ($page_id) {
+            $data = $this->get_page_info(array('page_id' => $page_id, 'status' => 1));
+            if ($data == null) {
                 $this->error404();
                 return;
             }
-
-            //Is the page published?
-            $date_now = new DateTime();
-            $pagePublishTime = DateTime::createFromFormat('Y-m-d H:i:s', $pageInfo->date_publish);
-
-            if ($date_now < $pagePublishTime) {
-                //Not found Page
-                $this->error404();
-                return;
-            }
-
-            $data['page'] = $pageInfo;
-            $data['meta'] = $this->getPageMetas($pageInfo);
-            $data['title'] = $pageInfo->page_data["title"] ? config("SITE_TITLE") . " - " . $pageInfo->page_data["title"] : config("SITE_TITLE") . " - " . $pageInfo->title;
-            $data['layout'] = $pageInfo->layout == 'default' ? 'site' : $pageInfo->layout;
-            $data['headers_includes'] = isset($pageInfo->page_data["headers_includes"]) ? $pageInfo->page_data["headers_includes"] : "";
-            $data['footer_includes'] = isset($pageInfo->page_data["footer_includes"]) ? $pageInfo->page_data["footer_includes"] : "";
-            $template = $pageInfo->template == 'default' ? 'template' : $pageInfo->template;
-
-            /* if (getThemePath()) {
-            $this->blade->changePath(getThemePath());
-            } */
-
-            $this->load->model('Admin/Menu');
-            $menu = new Menu();
-            $menu->find_with(['menu_id' => 1]);
-
-            $data["menu"] = $menu;
-
             if (getThemePath()) {
                 $this->blade->changePath(getThemePath());
             }
 
-            echo $this->blade->view("site.templates." . $template, $data);
-        }else{
+            echo $this->blade->view("site.templates." . $data["template"], $data);
+        } else {
+            // Show default
             $data['title'] = config("SITE_TITLE") . " - Home";
             if (getThemePath()) {
                 $this->blade->changePath(getThemePath());
@@ -117,29 +55,22 @@ class PageController extends Base_Controller
 
     public function preview()
     {
-        $pageInfo = new Page();
-        $result = $pageInfo->find_with(array('page_id' => $this->input->get('page_id')));
+        // Check user logged first
+        if (!$this->session->userdata('logged_in')) {
+            $uri = urlencode(uri_string());
+            redirect('/');
+        }
 
-        if (!$result) {
-            //Not found Page
+        $data = $this->get_page_info(array('page_id' => $this->input->get('page_id')));
+        if ($data == null) {
             $this->error404();
             return;
         }
-
-        $data['page'] = $pageInfo;
-        $data['meta'] = $this->getPageMetas($pageInfo);
-
-        $data['title'] = $pageInfo->page_data["title"] ? config("SITE_TITLE") . " - " . $pageInfo->page_data["title"] : config("SITE_TITLE") . " - " . $pageInfo->title;
-        $data['headers_includes'] = isset($pageInfo->page_data["headers_includes"]) ? $pageInfo->page_data["headers_includes"] : "";
-        $data['footer_includes'] = isset($pageInfo->page_data["footer_includes"]) ? $pageInfo->page_data["footer_includes"] : "";
-        $data['layout'] = $pageInfo->layout == 'default' ? 'site' : $pageInfo->layout;
-        $template = $pageInfo->template == 'default' ? 'template' : $pageInfo->template;
-
         if (getThemePath()) {
             $this->blade->changePath(getThemePath());
         }
 
-        echo $this->blade->view("site.templates." . $template, $data);
+        echo $this->blade->view("site.templates." . $data["template"], $data);
 
     }
 
@@ -148,6 +79,36 @@ class PageController extends Base_Controller
         $data['pages'] = $this->Page->where(["status" => 1]);
         header("Content-Type: text/xml;charset=iso-8859-1");
         echo $this->blade->view("admin.xml.sitemap", $data);
+    }
+
+    private function get_page_info($where)
+    {
+        $pageInfo = new Page();
+        $data['result'] = $pageInfo->find_with($where);
+        if (!$data['result']) {
+            //Not found Page
+            return null;
+        }
+        //Is the page published?
+        $date_now = new DateTime();
+        $data['pagePublishTime'] = DateTime::createFromFormat('Y-m-d H:i:s', $pageInfo->date_publish);
+
+        if ($date_now < $data['pagePublishTime']) {
+            return null;
+        }
+
+        $data['page'] = $pageInfo;
+        $data['meta'] = $this->getPageMetas($pageInfo);
+        $data['title'] = $pageInfo->page_data["title"] ? config("SITE_TITLE") . " - " . $pageInfo->page_data["title"] : config("SITE_TITLE") . " - " . $pageInfo->title;
+        $data['layout'] = $pageInfo->layout == 'default' ? 'site' : $pageInfo->layout;
+        $data['headers_includes'] = isset($pageInfo->page_data["headers_includes"]) ? $pageInfo->page_data["headers_includes"] : "";
+        $data['footer_includes'] = isset($pageInfo->page_data["footer_includes"]) ? $pageInfo->page_data["footer_includes"] : "";
+        $data['template'] = $pageInfo->template == 'default' ? 'template' : $pageInfo->template;
+        $this->load->model('Admin/Menu');
+        $menu = new Menu();
+        $menu->find_with(['menu_id' => 1]);
+        $data["menu"] = $menu;
+        return $data;
     }
 
 }
