6132c32ca35f305840e2653ebdd4210180caa7da
Gervis Bermudez <gervis.bermudez@tsoftlatam.com>
2022-12-28 01:13:15 -0300

    removing nice scroll

--- application/views/admin/shared/footer.blade.php
+++ application/views/admin/shared/footer.blade.php
@@ -6,16 +6,15 @@ const ENVIRONMENT = <?php echo json_encode(ENVIRONMENT) ?>;
 const DEBUGMODE = <?php echo json_encode($ci->config->item('debug_mode')) ?>;
 </script>
 @if (ENVIRONMENT == 'production'):
-	<script src="{{base_url(JSPATH . 'vue/vue.min.js?v=' . ADMIN_VERSION)}}"></script>
+<script src="{{base_url(JSPATH . 'vue/vue.min.js?v=' . ADMIN_VERSION)}}"></script>
 @else
-	<script src="{{base_url(JSPATH . 'vue/vue.js?v=' . ADMIN_VERSION)}}"></script>
+<script src="{{base_url(JSPATH . 'vue/vue.js?v=' . ADMIN_VERSION)}}"></script>
 @endif
 <script src="{{base_url(JSPATH . 'materialize.min.js?v=' . ADMIN_VERSION)}}"></script>
 <script src="{{base_url(JSPATH . 'jquery.js?v=' . ADMIN_VERSION)}}"></script>
-<script src="{{base_url(JSPATH . 'jquery.nicescroll.min.js?v=' . ADMIN_VERSION) }}"></script>
 <script src="{{base_url(JSPATH . 'start.min.js?v=' . ADMIN_VERSION)}}"></script>
 @isset($footer_includes)
-	@foreach($footer_includes as $include)
-	<?php echo $include ?>
-	@endforeach
+@foreach($footer_includes as $include)
+<?php echo $include ?>
+@endforeach
 @endisset
