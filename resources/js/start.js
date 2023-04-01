592242b44405cb6b7183c29a0de63af245ec4f38
Gervis Bermudez <gervis.bermudez@tsoftlatam.com>
2023-04-01 13:08:55 -0300

    fix: makeid function

--- resources/js/start.js
+++ resources/js/start.js
@@ -165,6 +165,7 @@ var mixins = {
       var characters =
         "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
       var charactersLength = characters.length;
+      let result = "";
       for (var i = 0; i < length; i++) {
         result += characters.charAt(
           Math.floor(Math.random() * charactersLength)
